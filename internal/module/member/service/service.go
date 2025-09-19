e4b8bcedc6331c417967475e15af59fe477b5ee4
Luvianto <126235306+Luvianto@users.noreply.github.com>
2025-09-19 11:50:31 +0700

    Fix: better error messages for memberService

--- internal/module/member/service/service.go
+++ internal/module/member/service/service.go
@@ -27,21 +27,21 @@ func NewMemberService(repo interfaces.MemberRepository) *memberService {
 func (s *memberService) GetAllMember() ([]*memberEntity.MemberGet, error) {
 	members, status, err := s.repo.GetAllMember()
 	if err != nil {
-		log.Error().Err(err).Msg("Gagal mendapatkan pengguna")
+		log.Error().Err(err).Msg("Gagal mendapatkan semua member")
 		return nil, err
 	}
 
 	if !status {
-		log.Error().Msg("Gagal mendapatkan pengguna")
-		return nil, fmt.Errorf("gagal mendapatkan pengguna")
+		log.Error().Msg("Gagal mendapatkan semua member")
+		return nil, fmt.Errorf("gagal mendapatkan semua member")
 	}
 
 	if len(members) == 0 {
-		log.Error().Msg("Pengguna tidak ditemukan")
-		return nil, fmt.Errorf("pengguna tidak ditemukan")
+		log.Error().Msg("Tidak ada member yang ditemukan")
+		return nil, fmt.Errorf("tidak ada member yang ditemukan")
 	}
 
-	log.Info().Msg("Berhasil mendapatkan pengguna")
+	log.Info().Msg("Berhasil mendapatkan semua member")
 
 	var memberGets []*memberEntity.MemberGet
 	for _, member := range members {
@@ -64,16 +64,16 @@ func (s *memberService) GetMember(req *memberEntity.MemberReqByID) (*memberEntit
 
 	member, found, err := s.repo.GetMember(req.ID)
 	if err != nil {
-		log.Error().Err(err).Msg("Gagal mendapatkan pengguna")
+		log.Error().Err(err).Msg("Gagal mendapatkan member")
 		return nil, err
 	}
 
 	if !found {
-		log.Error().Msg("Pengguna tidak ditemukan")
-		return nil, fmt.Errorf("pengguna tidak ditemukan")
+		log.Error().Msg("Member tidak ditemukan")
+		return nil, fmt.Errorf("member tidak ditemukan")
 	}
 
-	log.Info().Msg("Berhasil mendapatkan pengguna")
+	log.Info().Msg("Berhasil mendapatkan member")
 
 	return &memberEntity.MemberGet{
 		ID: member.ID,
@@ -99,7 +99,7 @@ func (s *memberService) CreateMember(req *memberEntity.MemberCreate) (*memberEnt
 
 	createUser, err := userUtils.UserCreateParser(&req.User, userUUID)
 	if err != nil {
-		log.Error().Err(err).Msg("Gagal memproses data pengguna")
+		log.Error().Err(err).Msg("Gagal memproses data member")
 		return nil, err
 	}
 
@@ -111,14 +111,16 @@ func (s *memberService) CreateMember(req *memberEntity.MemberCreate) (*memberEnt
 
 	member, status, err := s.repo.CreateMember(*createUser, *createMember)
 	if err != nil {
-		log.Error().Err(err).Msg("Gagal membuat pengguna")
+		log.Error().Err(err).Msg("Gagal membuat member")
 		return nil, err
 	}
 
 	if !status {
-		log.Error().Msg("Pengguna gagal dibuat")
-		return nil, fmt.Errorf("pengguna gagal dibuat")
+		log.Error().Msg("Member gagal dibuat")
+		return nil, fmt.Errorf("member gagal dibuat")
 	}
+
+	log.Info().Msg("Berhasil membuat member")
 
 	return &memberEntity.MemberGet{
 		ID: member.ID,
@@ -148,14 +150,16 @@ func (s *memberService) UpdateMember(req *memberEntity.MemberUpdate) (*memberEnt
 
 	member, status, err := s.repo.UpdateMember(*updateMember)
 	if err != nil {
-		log.Error().Err(err).Msg("Gagal mengubah pengguna")
+		log.Error().Err(err).Msg("Gagal mengubah member")
 		return nil, err
 	}
 
 	if !status {
-		log.Error().Msg("Pengguna gagal diubah")
-		return nil, fmt.Errorf("pengguna gagal diubah")
+		log.Error().Msg("Member gagal diubah")
+		return nil, fmt.Errorf("member gagal diubah")
 	}
+
+	log.Info().Msg("Berhasil mengubah member")
 
 	return &memberEntity.MemberGet{
 		ID: member.ID,
@@ -179,14 +183,16 @@ func (s *memberService) DeleteMember(req *memberEntity.MemberReqByID) (*memberEn
 
 	member, status, err := s.repo.DeleteMember(req.ID)
 	if err != nil {
-		log.Error().Err(err).Msg("Gagal menghapus pengguna")
+		log.Error().Err(err).Msg("Gagal menghapus member")
 		return nil, err
 	}
 
 	if !status {
-		log.Error().Msg("Pengguna gagal dihapus")
-		return nil, fmt.Errorf("pengguna gagal dihapus")
+		log.Error().Msg("Member gagal dihapus")
+		return nil, fmt.Errorf("member gagal dihapus")
 	}
+
+	log.Info().Msg("Berhasil menghapus member")
 
 	return &memberEntity.MemberGet{
 		ID: member.ID,
